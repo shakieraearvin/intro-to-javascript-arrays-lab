7a6124b6b2b8d417784c2625a720dc8cee9a3cca
Shakiera Earvin <kieraearvin@gmail.com>
2025-01-23 20:38:00 -0500

    exercise five and six done

--- app.js
+++ app.js
@@ -51,5 +51,28 @@ const favFood = foods[1];
 
 console.log('Exercise 4 result:', favFood);
 
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+foods.splice(1, 0, 'tofu')
 
 
+console.log('Exercise 5 result:', foods);
+
+
+/*
+Exercise 6: Replace elements
+
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+Complete Exercise 6 in the space below:
+*/
+
+foods.splice(2, 1, 'sushi', 'cupcake');
+
+console.log('Exercise 6 result:', foods);
